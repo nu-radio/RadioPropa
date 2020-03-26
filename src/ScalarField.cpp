18af80773d7dacd58197862c593cd4609820a555
carinakanitz <carina.kanitz@fau.de>
2020-03-26 10:59:55 +0100

    added ducts as possible models

--- src/ScalarField.cpp
+++ src/ScalarField.cpp
@@ -2,183 +2,220 @@
 
 namespace radiopropa {
 
-LinearIncrease::LinearIncrease(double _v0, const Vector3d &_g0) : v0(_v0), g0(_g0)
-{
+    LinearIncrease::LinearIncrease(double _v0, const Vector3d &_g0) : v0(_v0), g0(_g0) {
 
-}
+    }
 
-LinearIncrease::~LinearIncrease() {
-}
+    LinearIncrease::~LinearIncrease() {
+    }
 
-double LinearIncrease::getValue(const Vector3d &position) const
-{
-    return v0 * position.z;
-};
+    double LinearIncrease::getValue(const Vector3d &position) const {
+        return v0 * position.z;
+    };
 
-Vector3d LinearIncrease::getGradient(const Vector3d &position) const
-{
-  return Vector3d(0, 0, v0);
-};
+    Vector3d LinearIncrease::getGradient(const Vector3d &position) const {
+        return Vector3d(0, 0, v0);
+    };
 
 
+    GorhamIceModel::GorhamIceModel(double z0, double _a, double _b, double _c) : z0(z0), a(_a), b(_b), c(_c) {
 
-GorhamIceModel::GorhamIceModel(double z0, double _a, double _b, double _c) : z0(z0), a(_a), b(_b), c(_c)
-{
+    }
 
-}
+    GorhamIceModel::~GorhamIceModel() {
+    }
 
-GorhamIceModel::~GorhamIceModel()
-{
-}
-
-double GorhamIceModel::getValue(const Vector3d &position) const
-{
-	if (position.z-z0 < 0)
-      return a + b * (1.0 - exp(-1.*c*(position.z-z0)));
-	else
-      return 1.;
-
-};
-
-Vector3d GorhamIceModel::getGradient(const Vector3d &position) const
-{
-	Vector3d v(0,0,0);
-	if (position.z < 0)
-	{
-      v.z = 1.0 * b * c * exp(-1.*c*position.z);
-	}
-	// The gradient on discontinuities has to be 0 as these are handeled by a
-	// different module!
-	return v;
-}
-
-
-
-N_constant::N_constant(double _z0, double _n): z0(_z0), n(_n)
-{
-
-}
-
-double N_constant::getValue(const Vector3d &position) const
-{
-	if (position.z - z0 > 0)
-		return n;
-	else
-		return 1.0;
-}
-
-Vector3d N_constant::getGradient(const Vector3d &position) const
-{
-	return Vector3d(0,0,0);
-}
-
-
-
-Lin_grad::Lin_grad(double _z0, double _step_n): z0(_z0), step_n(_step_n)
-{
-
-}
-
-double Lin_grad::getValue(const Vector3d &position) const
-{
-	double d = position.z - z0;
-	if (d > 0)
-                return 1.0 + d*step_n;
+    double GorhamIceModel::getValue(const Vector3d &position) const {
+        if (position.z - z0 < 0)
+            return a + b * (1.0 - exp(-1. * c * (position.z - z0)));
         else
-                return 1.0;
+            return 1.;
 
-}
+    };
 
-Vector3d Lin_grad::getGradient(const Vector3d &position) const
-{
-       if (position.z - z0 > 0)
-	       return Vector3d(0, 0, step_n);
-       else
-	       return Vector3d(0,0,0);
-}
-
-
-
-CloudModel_atm::CloudModel_atm(double _z0, double _T0, double _p0, double _e): z0(_z0), T0(_T0), p0(_p0), e(_e)
-{
-
-}
-
-double CloudModel_atm::L = 6.5e-3;
-double CloudModel_atm::M = 0.02896; //kg/mol
-double CloudModel_atm::R = 8.314; //J/K/mol
-double CloudModel_atm::g = 9.807; //m/s**2
-double CloudModel_atm::D = 0.61121;
-double CloudModel_atm::a = 18.678;
-double CloudModel_atm::b = 234.5;
-double CloudModel_atm::c = 257.14;
+    Vector3d GorhamIceModel::getGradient(const Vector3d &position) const {
+        Vector3d v(0, 0, 0);
+        if (position.z < 0) {
+            v.z = 1.0 * b * c * exp(-1. * c * position.z);
+        }
+        // The gradient on discontinuities has to be 0 as these are handeled by a
+        // different module!
+        return v;
+    }
 
 
+    N_constant::N_constant(double _z0, double _n) : z0(_z0), n(_n) {
+
+    }
+
+    double N_constant::getValue(const Vector3d &position) const {
+        if (position.z - z0 > 0)
+            return n;
+        else
+            return 1.0;
+    }
+
+    Vector3d N_constant::getGradient(const Vector3d &position) const {
+        return Vector3d(0, 0, 0);
+    }
 
 
-double CloudModel_atm::getValue(const Vector3d &position) const
-{
+    Lin_grad::Lin_grad(double _z0, double _step_n) : z0(_z0), step_n(_step_n) {
 
-	double h = position.z;
-        double T = T0 - L*h;
-	double T_C = T - 273.15;
-        double P_air = p0*exp(-M*g*h/R/T);
-        double P_sat = D*exp((a-T_C/b)*(T_C/(c+T_C)))*10; //Bucks equation gives kPa
+    }
 
-	if (position.z - z0 > 0)
-	{
-		double N= 77.6*P_air/T + 3.73*1e5*P_sat/pow(T, 2);
-		return N*1e-6+1.0;
-	}
-	else
-	{
-		double N= 77.6*P_air/T + 3.73*1e5*e*P_sat/pow(T, 2);
-		return N*1e-6+1.0;
-	}
+    double Lin_grad::getValue(const Vector3d &position) const {
+        double d = position.z - z0;
+        if (d > 0)
+            return 1.0003 + d * step_n;
+        else
+            return 1.0003;
 
-}
+    }
 
-Vector3d CloudModel_atm::getGradient(const Vector3d &position) const
-{
-	double h = position.z;
-        double T = T0 - L*h;
-	double T_C = T - 273.15;
-        double P_air = p0*exp(-M*g*h/R/T);
-        double P_sat = D*exp((a-T_C/b)*(T_C/(c+T_C)))*10;
+    Vector3d Lin_grad::getGradient(const Vector3d &position) const {
+        if (position.z - z0 > 0)
+            return Vector3d(0, 0, step_n);
+        else
+            return Vector3d(0, 0, 0);
+    }
 
-	double dP_air = P_air *(-M*g/R)*T0/pow(T,2);
-	double dP_sat = -L* P_sat*T_C/(T_C+c)*(-2/b+a/T_C-(a-T_C/b)/(T_C+c));
+
+    CloudModel_atm::CloudModel_atm(double _z_bottom, double _z_top, double _T0, double _p0, double _e) : z_bottom(
+            _z_bottom), z_top(_z_top), T0(_T0), p0(_p0), e(_e) {
+    }
+
+    double CloudModel_atm::L = 6.5e-3;
+    double CloudModel_atm::M = 0.02896; //kg/mol
+    double CloudModel_atm::R = 8.314; //J/K/mol
+    double CloudModel_atm::g = 9.807; //m/s**2
+    double CloudModel_atm::D = 6.1121;
+    double CloudModel_atm::a = 18.678;
+    double CloudModel_atm::b = 234.5;
+    double CloudModel_atm::c = 257.14;
+
+
+    double CloudModel_atm::getValue(const Vector3d &position) const {
+
+        double h = position.z;
+        double T = T0 - L * h;
+        double T_C = T - 273.15;
+        double P_air = p0 * pow(T/T0,g*M/L/R);
+        double P_sat = D * exp((a - T_C / b) * (T_C / (c + T_C))); //Bucks equation
+
+        if (z_bottom < position.z && position.z < z_top) {
+            double N = 77.6 * P_air / T - 5.6 * P_sat/T + 3.75 * 1e5 * P_sat / pow(T, 2);
+            return N * 1e-6 + 1.0;
+        } else {
+            double N = 77.6 * P_air / T -5.6 * e * P_sat/T + 3.75 * 1e5 * e * P_sat / pow(T, 2);
+            return N * 1e-6 + 1.0;
+        }
+
+    }
+
+    Vector3d CloudModel_atm::getGradient(const Vector3d &position) const {
+        double h = position.z;
+        double T = T0 - L * h;
+        double T_C = T - 273.15;
+        double P_air = p0 * pow(T/T0,g*M/L/R);
+        double P_sat = D * exp((a - T_C / b) * (T_C / (c + T_C)));
+
+        double dP_air = - P_air /T *M*g/R;
+        double dP_sat = -L * P_sat * T_C / (T_C + c) * (-2 / b + a / T_C - (a - T_C / b) / (T_C + c));
         double dT = -L;
 
-	if (position.z - z0 > 0)
-	{
-		double dN= 77.6*(1/T*dP_air - P_air/pow(T,2)*dT) + 3.73*1e5*(1/pow(T,2)*dP_sat-2*P_sat/pow(T,3)*dT);
-		double dn =  dN*1e-6;
-		return Vector3d(0,0,dn);
-	}
-	else
-	{
-		double dN= 77.6*(1/T*dP_air - P_air/pow(T,2)*dT) + 3.73*1e5*(1/pow(T,2)*e*dP_sat-2*e*P_sat/pow(T,3)*dT);
-                double dn =  dN*1e-6;
-		return Vector3d(0,0,dn);
-	}
+        if (z_bottom < position.z && position.z < z_top) {
+            double dN = 77.6 * (1 / T * dP_air - P_air / pow(T, 2) * dT) - 5.6 *(1/T*dP_sat - 1/pow(T,2)*P_sat*dT) +
+                        3.75 * 1e5 * (1 / pow(T, 2) * dP_sat - 2 * P_sat / pow(T, 3) * dT);
+            double dn = dN * 1e-6;
+            return Vector3d(0, 0, dn);
+        } else {
+            double dN = 77.6 * (1 / T * dP_air - P_air / pow(T, 2) * dT) - 5.6 *(1/T*e*dP_sat - 1/pow(T,2)*e*P_sat*dT)+
+                        3.75 * 1e5 * (1 / pow(T, 2) * e * dP_sat - 2 * e * P_sat / pow(T, 3) * dT);
+            double dn = dN * 1e-6;
+            return Vector3d(0, 0, dn);
+        }
+    }
+
+
+    n2linear::n2linear(double _n0, double _a) : n0(_n0), a(_a) {}
+
+    n2linear::~n2linear() {};
+
+    double n2linear::getValue(const Vector3d &position) const {
+        return sqrt(n0 * n0 + a * position.z);
+    }
+
+    Vector3d n2linear::getGradient(const Vector3d &position) const {
+        return Vector3d(0, 0, a / 2. / getValue(position));
+    }
+
+    surfaceDuct::surfaceDuct() {}
+
+    double surfaceDuct::getValue(const Vector3d &position) const{
+           if (position.z < 64.2){
+               double M =  300 - 8.74/64.21*position.z;
+               double N = M - 157.0/1000*position.z;
+               return N*1e-6 + 1;
+           }
+           else{
+               double M = 300 - 8.74 + 117.0/1000*(position.z - 64.21);
+               double N = M - 157.0/1000*position.z;
+               return N*1e-6 + 1;
+           }
+    }
+
+    Vector3d surfaceDuct::getGradient(const Vector3d &position) const{
+        if (position.z < 64.2){
+            double dM = -8.74/64.21;
+            double dN =  dM - 157.0/1000.0;
+            return Vector3d(0,0,dN*1e-6);
+        }
+        else{
+            double dN = -40;
+            return Vector3d(0,0,dN*1e-9);
+        }
+    }
+
+    elevatedDuct::elevatedDuct(){}
+
+    double elevatedDuct::getValue(const Vector3d &position) const{
+        double M_e = 9.29;
+        double u = 1166.48;
+        double b = 1100.25;
+        double th = 147.81;
+        double dM = 117.0/1000;
+        double M;
+        double dm_1 = M_e/(u-b);
+        double dm_2 = M_e/(b + th -u);
+        if (position.z < 1166.48){
+            M = 300 + dm_1*position.z;
+        }
+        else if(position.z < 1248.06){
+            M = 300 + dm_1*u - dm_2*(position.z - u);
+        }
+        else {
+            M = 300 + dm_1*u - dm_2*(b + th - u) + dM*(position.z - b - th);
+        }
+        double N = M - 157.0/1000*position.z;
+        return N*1e-6 + 1;
+    }
+
+    Vector3d elevatedDuct::getGradient(const Vector3d &position) const{
+        double dm;
+        if(position.z < 1166.48){
+            dm = 9.29/(1166.48 - 1100.25);
+        }
+        else if(position.z < 1248.06){
+            dm = -9.29/(1248.06 - 1166.48);
+        }
+        else{
+            dm = 117.0/1000;
+        }
+        double dN = dm - 157.0/1000;
+
+        return Vector3d(0,0,dN*1e-6);
+    }
+
 }
-
-
-
-n2linear::n2linear(double _n0, double _a) : n0(_n0), a(_a) { }
-
-n2linear::~n2linear() { };
-
-double n2linear::getValue(const Vector3d &position) const
-{
-    return sqrt(n0*n0 + a * position.z);
-}
-
-Vector3d n2linear::getGradient(const Vector3d &position) const
-{
-		return Vector3d(0,0,a/2. / getValue(position));
-}
-
-
-} // namespace
+// namespace
